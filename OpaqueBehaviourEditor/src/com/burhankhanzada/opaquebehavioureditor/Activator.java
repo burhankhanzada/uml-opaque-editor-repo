fcff6557c753ee48bb787ec2bc6c33acc890bb3a
burhankhanzada <burhankhanzada199888@gmail.com>
2026-06-09 17:41:19 +0200

    refactor: standardize plugin ID and activator package naming while updating project configuration and git ignore rules

--- OpaqueBehaviourEditor/src/com/burhankhanzada/opaquebehavioureditor/Activator.java
+++ OpaqueBehaviourEditor/src/com/burhankhanzada/opaquebehavioureditor/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 
 public class Activator extends AbstractUIPlugin {
 
-	public static final String PLUGIN_ID = "UMLOpaqueBehaviourBodyEditor";
+	public static final String PLUGIN_ID = "com.burhankhanzada.opaquebehavioureditor";
 
 	private static Activator plugin;
   
